ff5dd40dc871e1f28f8f7a25414d8f578e13f1cb
songhn233 <47357585+songhn233@users.noreply.github.com>
2020-02-27 23:22:18 +0800

    概率期望

---
+++ hdu-winter-2020/problems/期望概率/1001.cpp
@@ -0,0 +1,35 @@
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<cmath>
+#include<map>
+#include<set>
+#define ll long long
+#define F(i,a,b) for(int i=(a);i<=(b);i++)
+#define mst(a,b) memset((a),(b),sizeof(a))
+#define PII pair<int,int>
+using namespace std;
+template<class T>inline void read(T &x) {
+    x=0; int ch=getchar(),f=0;
+    while(ch<'0'||ch>'9'){if (ch=='-') f=1;ch=getchar();}
+    while (ch>='0'&&ch<='9'){x=(x<<1)+(x<<3)+(ch^48);ch=getchar();}
+    if(f)x=-x;
+}
+const int inf=0x3f3f3f3f;
+int k;
+double p;
+int main()
+{
+	while(cin>>k)
+	{
+		if(k==0) break;
+		cin>>p;
+		double t=k;
+		printf("%.3f %.3f\n",t/p,t*((t+1)/(p*p)-(1/p)));
+	}
+	return 0;
+}
+
