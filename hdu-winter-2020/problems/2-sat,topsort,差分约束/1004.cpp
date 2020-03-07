b410caf17e22e6c5d2b023ed77ab420ac60c2e9e
songhn233 <47357585+songhn233@users.noreply.github.com>
2020-03-07 14:33:07 +0800

    add 中国剩余 差分约束

--- hdu-winter-2020/problems/2-sat,topsort,差分约束/1004.cpp
+++ hdu-winter-2020/problems/2-sat,topsort,差分约束/1004.cpp
@@ -13,10 +13,10 @@
 #define PII pair<int,int>
 using namespace std;
 template<class T>inline void read(T &x) {
-    x=0; int ch=getchar(),f=0;
-    while(ch<'0'||ch>'9'){if (ch=='-') f=1;ch=getchar();}
-    while (ch>='0'&&ch<='9'){x=(x<<1)+(x<<3)+(ch^48);ch=getchar();}
-    if(f)x=-x;
+  x=0; int ch=getchar(),f=0;
+  while(ch<'0'||ch>'9'){if (ch=='-') f=1;ch=getchar();}
+  while (ch>='0'&&ch<='9'){x=(x<<1)+(x<<3)+(ch^48);ch=getchar();}
+  if(f)x=-x;
 }
 const int inf=0x3f3f3f3f;
 const int maxn=250050;
