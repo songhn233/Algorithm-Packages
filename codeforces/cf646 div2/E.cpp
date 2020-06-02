96af69f8b75cb7af059a7ecd96c25dcec845da8b
songhn233 <47357585+songhn233@users.noreply.github.com>
2020-06-02 18:54:45 +0800

    add cf646 div2 E

--- codeforces/cf646 div2/E.cpp
+++ codeforces/cf646 div2/E.cpp
@@ -8,6 +8,7 @@
 #include<map>
 #include<set>
 #define ll long long
+#define int ll
 #define F(i,a,b) for(int i=(a);i<=(b);i++)
 #define mst(a,b) memset((a),(b),sizeof(a))
 #define PII pair<int,int>
@@ -21,26 +22,17 @@ template<class T>inline void rd(T &x) {
     if(f)x=-x;
 }
 const int inf=0x3f3f3f3f;
-const int maxn=400050;
-int n,t1,t2,p1,p2,num,head[maxn];
-int a[maxn],b[maxn],c[maxn],s1[maxn],s2[maxn];
-struct node{
-    int v,nex;
-}e[maxn];
-void add(int u,int v)
-{
-    e[++num].nex=head[u];
-    e[num].v=v;
-    head[u]=num;
-}
+const int maxn=200050;
+ll n,t1,t2,p1,p2;
+ll a[maxn],b[maxn],c[maxn],s1[maxn],s2[maxn];
+vector<int> e[maxn];
 ll ans=0;
-void dfs(int u,int fa,int t)
+void dfs(int u,int fa,ll t)
 {
     t=min(t,a[u]);
-    s1[u]=s2[u]=0;
-    for(int i=head[u];i;i=e[i].nex)
+    for(int i=0;i<e[u].size();i++)
     {
-        int v=e[i].v;
+        int v=e[u][i];
         if(v==fa) continue;
         dfs(v,u,t);
         s1[u]+=s1[v];
@@ -48,9 +40,13 @@ void dfs(int u,int fa,int t)
     }
     if(b[u]==1&&c[u]==0) s1[u]++;
     if(b[u]==0&&c[u]==1) s2[u]++;
-    ans+=2ll*min(s1[u],s2[u])*t;
-    s1[u]-=min(s1[u],s2[u]);
-    s2[u]-=min(s1[u],s2[u]);
+    if(a[u]==t)
+    {
+        ll tt=min(s1[u],s2[u]);
+        ans+=2*tt*t;
+        s1[u]-=tt;
+        s2[u]-=tt;
+    }
 }
 signed main()
 {
@@ -64,13 +60,12 @@ signed main()
     rep(i,1,n-1)
     {
         int u,v;rd(u),rd(v);
-        add(u,v);
-        add(v,u);
+        e[u].push_back(v);
+        e[v].push_back(u);
     }
     if(t1!=t2) puts("-1");
     else
     {
-        ans=0;
         dfs(1,0,inf);
         cout<<ans<<endl;
     }
