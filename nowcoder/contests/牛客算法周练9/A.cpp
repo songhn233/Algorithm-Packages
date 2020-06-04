49995606d22b69cebb679df35934884590b1a2b6
songhn233 <47357585+songhn233@users.noreply.github.com>
2020-06-04 22:19:33 +0800

    add 牛客算法周练9

---
+++ nowcoder/contests/牛客算法周练9/A.cpp
@@ -0,0 +1,34 @@
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<cmath>
+#include<map>
+#include<set>
+#define ll long long
+#define F(i,a,b) for(int i=(a);i<=(b);i++)
+#define mst(a,b) memset((a),(b),sizeof(a))
+#define PII pair<int,int>
+#define rep(i,x,y) for(auto i=(x);i<=(y);++i)
+#define dep(i,x,y) for(auto i=(x);i>=(y);--i)
+using namespace std;
+template<class T>inline void rd(T &x) {
+    x=0; int ch=getchar(),f=0;
+    while(ch<'0'||ch>'9'){if (ch=='-') f=1;ch=getchar();}
+    while (ch>='0'&&ch<='9'){x=(x<<1)+(x<<3)+(ch^48);ch=getchar();}
+    if(f)x=-x;
+}
+const int inf=0x3f3f3f3f;
+ll n,m;
+int main()
+{
+    cin>>n>>m;
+    ll k=(1ll<<n);
+    ll p=k%7;
+    ll kk=((1-p)%7+7)%7;
+    ll num=(k-kk)/7ll;
+    cout<<max(num,0ll)<<endl;
+    return 0;
+}
---
+++ nowcoder/contests/牛客算法周练9/E.cpp
@@ -0,0 +1,135 @@
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<cmath>
+#include<map>
+#include<set>
+#define ll long long
+#define F(i,a,b) for(int i=(a);i<=(b);i++)
+#define mst(a,b) memset((a),(b),sizeof(a))
+#define PII pair<int,int>
+#define rep(i,x,y) for(auto i=(x);i<=(y);++i)
+#define dep(i,x,y) for(auto i=(x);i>=(y);--i)
+using namespace std;
+template<class T>inline void rd(T &x) {
+    x=0; int ch=getchar(),f=0;
+    while(ch<'0'||ch>'9'){if (ch=='-') f=1;ch=getchar();}
+    while (ch>='0'&&ch<='9'){x=(x<<1)+(x<<3)+(ch^48);ch=getchar();}
+    if(f)x=-x;
+}
+const int inf=0x3f3f3f3f;
+const int maxn=205;
+struct Wint:vector<int>
+{
+    Wint(int n=0)
+    {
+        push_back(n);
+        check();
+    }
+    Wint& check()
+    {
+        while(!empty()&&!back())pop_back();
+        if(empty())return *this;
+        for(int i=1; i<size(); ++i)
+        {
+            (*this)[i]+=(*this)[i-1]/10;
+            (*this)[i-1]%=10;
+        }
+        while(back()>=10)
+        {
+            push_back(back()/10);
+            (*this)[size()-2]%=10;
+        }
+        return *this;
+    }
+};
+istream& operator>>(istream &is,Wint &n)
+{
+    string s;
+    is>>s;
+    n.clear();
+    for(int i=s.size()-1; i>=0; --i)n.push_back(s[i]-'0');
+    return is;
+}
+ostream& operator<<(ostream &os,const Wint &n)
+{
+    if(n.empty())os<<0;
+    for(int i=n.size()-1; i>=0; --i)os<<n[i];
+    return os;
+}
+bool operator!=(const Wint &a,const Wint &b)
+{
+    if(a.size()!=b.size())return 1;
+    for(int i=a.size()-1; i>=0; --i)
+        if(a[i]!=b[i])return 1;
+    return 0;
+}
+bool operator==(const Wint &a,const Wint &b)
+{
+    return !(a!=b);
+}
+bool operator<(const Wint &a,const Wint &b)
+{
+    if(a.size()!=b.size())return a.size()<b.size();
+    for(int i=a.size()-1; i>=0; --i)
+        if(a[i]!=b[i])return a[i]<b[i];
+    return 0;
+}
+bool operator>(const Wint &a,const Wint &b)
+{
+    return b<a;
+}
+bool operator<=(const Wint &a,const Wint &b)
+{
+    return !(a>b);
+}
+bool operator>=(const Wint &a,const Wint &b)
+{
+    return !(a<b);
+}
+Wint& operator+=(Wint &a,const Wint &b)
+{
+    if(a.size()<b.size())a.resize(b.size());
+    for(int i=0; i!=b.size(); ++i)a[i]+=b[i];
+    return a.check();
+}
+Wint operator+(Wint a,const Wint &b)
+{
+    return a+=b;
+}
+Wint operator*(const Wint &a,const Wint &b)
+{
+    Wint n;
+    n.assign(a.size()+b.size()-1,0);
+    for(int i=0; i!=a.size(); ++i)
+        for(int j=0; j!=b.size(); ++j)
+            n[i+j]+=a[i]*b[j];
+    return n.check();
+}
+Wint f[maxn][maxn];
+Wint a[maxn];
+int n;
+int main()
+{
+    cin>>n;
+    Wint t=1e9;Wint p=100;
+    for(int i=0;i<=2*n;i++) for(int j=0;j<=2*n;j++) f[i][j]=p*t*t*t;
+    rep(i,1,n) cin>>a[i],a[i+n]=a[i];
+    for(int i=1;i<=2*n-2;i++) f[i][i+1]=0;
+    for(int i=1;i<=2*n-2;i++) f[i][i+2]=a[i]*a[i+1]*a[i+2];
+    for(int len=4;len<=n;len++)
+    {
+        for(int l=1;l<=2*n-len+1;l++)
+        {
+            int r=l+len-1;
+            for(int k=l+1;k<=r-1;k++) f[l][r]=min(f[l][r],f[l][k]+f[k][r]+a[l]*a[k]*a[r]);
+        }
+    }
+    Wint ans=f[1][n];
+    for(int i=1;i<=n+1;i++) ans=min(ans,f[i][i+n-1]);
+    cout<<ans<<endl;
+    return 0;
+}
