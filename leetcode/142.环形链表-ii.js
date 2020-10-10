57794e2b095f34401a057bbc4ccc674b9c89d0a8
songhn233 <47357585+songhn233@users.noreply.github.com>
2020-10-10 21:44:41 +0800

    add: leetcode142

---
+++ leetcode/142.环形链表-ii.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=javascript
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const detectCycle = (head) => {
+  const map = new Map()
+  while (head) {
+    if (map.get(head)) {
+      return head
+    }
+    map.set(head, true)
+    head = head.next
+  }
+  return null
+}
+// @lc code=end
