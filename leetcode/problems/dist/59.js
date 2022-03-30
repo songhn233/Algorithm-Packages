72b4d0c397c578a32bb745ab259539aa9732c305
songhn233 <songhn233@gmail.com>
2022-03-31 02:28:14 +0800

    daily update

---
+++ leetcode/problems/dist/59.js
@@ -0,0 +1,29 @@
+function generateMatrix(n) {
+    var len = n - 1, time = Math.floor(n / 2);
+    var sx = 0, sy = 0, x = 0, y = 0, num = 1;
+    var ans = Array(n)
+        .fill(0)
+        .map(function () { return Array(n).fill(0); });
+    while (time--) {
+        ;
+        (x = sx), (y = sy);
+        while (x - sx < len) {
+            ans[y][x++] = num++;
+        }
+        while (y - sy < len) {
+            ans[y++][x] = num++;
+        }
+        while (x > sx) {
+            ans[y][x--] = num++;
+        }
+        while (y > sy) {
+            ans[y--][x] = num++;
+        }
+        ++sx, ++sy;
+        len -= 2;
+    }
+    if (n & 1) {
+        ans[sx][sy] = num;
+    }
+    return ans;
+}
