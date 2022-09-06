5a742cebddb5279af3c265c07386de436a4192ff
songhn233 <songhn233@gmail.com>
2022-09-07 04:22:32 +0800

    daily update

--- leetcode/problems/offer/59-1.ts
+++ leetcode/problems/offer/59-1.ts
@@ -6,11 +6,11 @@ function maxSlidingWindow(nums: number[], k: number): number[] {
   const ans: number[] = []
   for (let i = 0; i < n; i++) {
     if (i >= k) {
-      if (q[l] < i - k + 1) {
+      while (l <= r && q[l] < i - k + 1) {
         l++
       }
     }
-    while (r - l + 1 >= 1 && nums[q[r]] <= nums[i]) {
+    while (l <= r && nums[q[r]] <= nums[i]) {
       r--
     }
     q[++r] = i
